2dbbc4ed5ad9e92650732096ffa36e3c301290d5
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2026-06-16 03:11:08 +0300

    update home page

--- app/(public)/home/page.tsx
+++ app/(public)/home/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
         <HeroSection />
         <FeaturesSection />
         <BenefitsSection />
-        <StatsSection />
+        {/* <StatsSection /> */}
       </div>
     </>
   );
